eb64df660dff7a5e337384f3ee948c53b42772e7
Jhonatan Santiago <jhonssantiago@gmail.com>
2026-09-25 14:48:21 -0300

    Project initiation and configuration

---
+++ src/main/java/com/jhonswolf/fantasticpets/FantasticPets.java
@@ -0,0 +1,23 @@
+package com.jhonswolf.fantasticpets;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import software.bernie.geckolib.GeckoLib;
+
+// Define a classe principal do mod e vincula ao ID
+@Mod(FantasticPets.MOD_ID)
+public class FantasticPets {
+
+    // Armazena o ID principal do mod
+    public static final String MOD_ID = "fantasticpets";
+
+    // Construtor principal da classe
+    public FantasticPets() {
+        // Obtem o event bus do ciclo de vida do mod
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Inicializa o motor de animacoes GeckoLib
+        GeckoLib.initialize();
+    }
+}
